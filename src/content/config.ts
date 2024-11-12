2415e177961ac5f028abccd62e673d538b6efb64
rafay99-epic <99marafay@gmail.com>
2024-11-12 12:51:43 +0500

    v1

---
+++ src/content/config.ts
@@ -0,0 +1,48 @@
+import { defineCollection, z } from "astro:content";
+
+
+// Post collection schema
+const postsCollection = defineCollection({
+  schema: z.object({
+    id: z.string().optional(),
+    title: z.string(),
+    meta_title: z.string().optional(),
+    description: z.string().optional(),
+    date: z.date().optional(),
+    image: z.string().optional(),
+    authors: z.array(z.string()).default(["admin"]),
+    categories: z.array(z.string()).default(["others"]),
+    tags: z.array(z.string()).default(["others"]),
+    draft: z.boolean().optional(),
+  }),
+});
+
+// Author collection schema
+const authorsCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    image: z.string().optional(),
+    description: z.string().optional(),
+    draft: z.boolean().optional(),
+  }),
+});
+
+// Pages collection schema
+const pagesCollection = defineCollection({
+  schema: z.object({
+    id: z.string().optional(),
+    title: z.string(),
+    meta_title: z.string().optional(),
+    description: z.string().optional(),
+    image: z.string().optional(),
+    layout: z.string().optional(),
+    draft: z.boolean().optional(),
+  }),
+});
+
+// Export collections
+export const collections = {
+  posts: postsCollection,
+  pages: pagesCollection,
+  authors: authorsCollection,
+};
